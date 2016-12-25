e1b9e7d33423247c3832d8275108288e08bac5b7
Ehsan Mehralian <ehsan.mehralian@gmail.com>
2016-12-26 02:13:56 +0330

    slider and background image implimented

--- js/home.js
+++ js/home.js
@@ -2,4 +2,17 @@
  * Created by ehsan on 12/23/16.
  */
 
+$(document).ready(function() {
+    $("#slider").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 4,
+        pagination:false
+
+        });
+
+
+
+    $("#firstViewPoint").css("height",$(window).height()-100)
+                        .css("background-image","url('../images/call-of-duty-background-18.jpg')");
+});
 
