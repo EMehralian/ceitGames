9a1e27615c4316a3776fb33612af846af16995ed
Ehsan Mehralian <ehsan.mehralian@gmail.com>
2016-12-26 02:47:41 +0330

    some changes!

--- js/home.js
+++ js/home.js
@@ -9,10 +9,23 @@ $(document).ready(function() {
         pagination:false
 
         });
-
-
-
     $("#firstViewPoint").css("height",$(window).height()-100)
                         .css("background-image","url('../images/call-of-duty-background-18.jpg')");
+
+    // $(".item").html()
+
+    // $.getJSON( "‫‪api.ie.ce-it.ir/F95/home‬‬", function( data ) {
+    //     var items = [];
+    //     $.each( data, function( key, val ) {
+    //         items.push( "<li id='" + key + "'>" + val + "</li>" );
+    //     });
+    //
+    //     $( "<ul/>", {
+    //         "class": "my-new-list",
+    //         html: items.join( "" )
+    //     }).appendTo( "body" );
+    // });
+
 });
+
 
