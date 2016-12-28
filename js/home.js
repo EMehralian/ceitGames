811ef4d8945c271002a5786dae0fdbf9d4d4e750
Ehsan Mehralian <ehsan.mehralian@gmail.com>
2016-12-28 05:04:09 +0330

    going to impliment new games

--- js/home.js
+++ js/home.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
     $("#firstViewPoint").css("height",$(window).height()-100)
                         .css("background-image","url('../images/call-of-duty-background-18.jpg')");
 
+    $(".item").each(function(i, obj) {
+        $(obj).css("height",100)
+            .css("background-image","url('../images/421645.jpg')");
+    });
+
+    $("#newGamesSlider").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 4,
+        pagination:false
+
+    });
+
     // $(".item").html()
 
     // $.getJSON( "‫‪api.ie.ce-it.ir/F95/home‬‬", function( data ) {
@@ -27,5 +39,44 @@ $(document).ready(function() {
     // });
 
 });
+
+// $('.item')
+//     .on('mouseover', function(e) {
+//         e.preventDefault();
+//         $(this).hover(function () {
+//             $(this).css("background-color","yellow");
+//         });
+//         console.log($(this).index());
+//     })
+//
+
+$('#slider')
+    .on('mouseover', function(e) {
+        var carousel = $('.owl-carousel').data('owl.carousel');
+        e.preventDefault();
+        $(this).hover(function () {
+            $(this).css("background-color","yellow");
+        });
+        console.log(carousel);
+    })
+    // .on('mouseout', '.owl-item', function(e) {
+    //     var carousel = $('.owl-carousel').data('owl.carousel');
+    //     e.preventDefault();
+    //     $(this).hover(function () {
+    //         $(this).css("background-color","");
+    //     });
+    //     console.log($(this).index());
+    // })
+    .on('click', '.item', function(e) {
+        var carousel = $('.owl-carousel').data('owl.carousel');
+        e.preventDefault();
+        var bg = $(this).css('background-image');
+        bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+        $("#firstViewPoint").css("height",$(window).height()-100)
+            .css("background-image","url("+bg+")");
+        console.log(bg);
+
+    })
+;
 
 
