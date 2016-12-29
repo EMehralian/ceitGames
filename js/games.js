bf443fc4d1ebfb09f61a8c86b4b42022bcae4cec
Ehsan Mehralian <ehsan.mehralian@gmail.com>
2016-12-29 15:14:32 +0330

    overal view compited

--- js/games.js
+++ js/games.js
@@ -2,7 +2,172 @@
  * Created by ehsan on 12/28/16.
  */
 
-function openCity(evt, tabName) {
+// ----------------------------------------header--------------------------------
+var Info=document.createElement("div");
+Info.className="Info";
+var nam=document.createElement("div");
+nam.className="name";
+nam.innerHTML="Batterfiled 4: Dragon's Theeth";
+var categury=document.createElement("div");
+for(i=0;i<3;i++){
+    var sp = document.createElement("span");
+    sp.innerHTML="اکشن،";
+    categury.appendChild(sp);
+}
+var gamerate= document.createElement("div");
+gamerate.className="gamerate";
+var rate=document.createElement("div");
+rate.className="buttomRate";
+for(var j=0; j<5;j++){
+    var star= document.createElement("i");
+    star.className="fa fa-star";
+    if(j<3)
+        star.className +=" blue-star";
+    rate.appendChild(star);
+}
+var score= document.createElement("div");
+score.className="score";
+score.innerHTML=" ۴.۳(۸۴رای)";
+gamerate.appendChild(rate);
+gamerate.appendChild(score);
+sp= document.createElement("span");
+sp.className="imageSpan";
+img = document.createElement("img");
+img.src="../images/call-of-duty-background-17.jpg";
+sp.appendChild(img);
+Info.appendChild(nam);
+Info.appendChild(categury);
+Info.appendChild(gamerate);
+document.getElementById("container").appendChild(Info);
+document.getElementById("container").appendChild(sp);
+
+// -----------------------------اطلاعات بازی---------------------------------------------
+
+var p = document.createElement("p");
+p.innerHTML="این اطلاعات اطلاعات خوبی است";
+var im = document.createElement("div");
+im.className="image";
+var img= document.createElement("img");
+img.src="../images/assassins-creed-3-wallpaper-3.jpg";
+im.appendChild(img);
+var p2 = document.createElement("p");
+p2.innerHTML="این اطلاعات اطلاعات خوبی است";
+document.getElementById("gameInfo").appendChild(p);
+document.getElementById("gameInfo").appendChild(im);
+document.getElementById("gameInfo").appendChild(p2);
+
+// -------------------------------------------نظرات کاربران----------------------------------
+
+for(i=0 ; i< 5; i++){
+    comment=document.createElement("div");
+    comment.className="comment";
+    sp= document.createElement("span");
+    sp.className="imageSpan";
+    img = document.createElement("img");
+    img.src="../assets/user.png";
+    sp.appendChild(img);
+    var info= document.createElement("div");
+    info.className="info";
+    var date = document.createElement("div");
+    date.className="date";
+    date.innerHTML="۲۲ آذر ۹۵";
+    nam= document.createElement("div");
+    nam.className="name";
+    nam.innerHTML="ali:";
+    var message = document.createElement("div");
+    message.className="message";
+    message.innerHTML="راهنمای بازی ۱";
+    info.appendChild(date);
+    info.appendChild(nam);
+    info.appendChild(message);
+    if(i % 2 == 0){
+        rate=document.createElement("div");
+        rate.className="rate";
+        for(var j=0; j<5;j++){
+            star= document.createElement("i");
+            star.className="fa fa-star";
+            if(j<3)
+                star.className +=" gray-star";
+            rate.appendChild(star);
+        }
+        info.appendChild(rate);
+    }
+    comment.appendChild(sp);
+    comment.appendChild(info);
+    document.getElementById("comments").appendChild(comment);
+}
+
+// ---------------------------------------بازی های مشابه-----------------------------------------------
+
+for(i=0 ; i<8; i++) {  ////items in the newGame slider
+    item = document.createElement("div");
+    item.className = "newGamesItem";
+    img= document.createElement("img");
+    img.style.height="100px";
+    img.src=("../images/ogImage.img.jpg");
+    var inf= document.createElement("div");
+    inf.className="Info";
+    nam=document.createElement("div");
+    nam.className="name";
+    nam.innerHTML="FIFA 2016";
+
+    categury=document.createElement("div");
+    categury.className="categury";
+    categury.innerHTML="ورزشی";
+    var Rate = document.createElement("div");
+    Rate.className="buttomRate";
+    for(j=0; j<5;j++){
+        star= document.createElement("i");
+        star.className="fa fa-star";
+        if(j<3)
+            star.className +=" blue-star";
+        Rate.appendChild(star);
+    }
+    inf.appendChild(nam);
+    inf.appendChild(categury);
+    inf.appendChild(Rate);
+    item.appendChild(img);
+    item.appendChild(inf);
+    document.getElementById("similarContainer").appendChild(item);
+}
+
+// ----------------------------عکس ها و ویدیو ها---------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    $("#Slider").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items: 4,
+        pagination: true,
+        navigationText: [
+            "<i class='fa fa-chevron-left'></i>",
+            "<i class='fa fa-chevron-right'></i>"
+        ]
+
+    });
+});
+
+
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -15,3 +180,6 @@ function openCity(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+
--- js/home.js
+++ js/home.js
@@ -2,6 +2,105 @@
  * Created by ehsan on 12/23/16.
  */
 
+
+for(var i=0 ; i<5; i++) {  ////items in the main slider
+    var item = document.createElement("div");
+    item.className = "item";
+    var div=document.createElement("div");
+    div.className = "itemblur";
+    item.appendChild(div);
+    var p =document.createElement("p");
+    p.className = "itemTitle";
+    p.innerHTML="بررسی فلان بازی";
+    item.appendChild(p);
+    document.getElementById("slider").appendChild(item);
+}
+
+
+for(i=0 ; i<8; i++) {  ////items in the newGame slider
+    item = document.createElement("div");
+    item.className = "newGamesItem";
+    sp= document.createElement("span");
+    sp.className="image";
+    img = document.createElement("img");
+    img.src="../images/ogImage.img.jpg";
+    sp.appendChild(img);
+
+    var inf= document.createElement("div");
+    inf.className="Info";
+    var nam=document.createElement("div");
+    nam.className="name";
+    nam.innerHTML="FIFA 2016";
+
+    var categury=document.createElement("div");
+    categury.className="categury";
+    categury.innerHTML="ورزشی";
+    var Rate = document.createElement("div");
+    Rate.className="buttomRate";
+    for(var j=0; j<5;j++){
+        var star= document.createElement("i");
+        star.className="fa fa-star";
+        if(j<3)
+            star.className +=" blue-star";
+
+        Rate.appendChild(star);
+    }
+    inf.appendChild(nam);
+    inf.appendChild(categury);
+    inf.appendChild(Rate);
+    item.appendChild(sp);
+    item.appendChild(inf);
+    document.getElementById("newGamesSlider").appendChild(item);
+}
+
+
+
+for(i=0 ; i< 5; i++){
+    var comment=document.createElement("div");
+    comment.className="comment";
+    sp= document.createElement("span");
+    sp.className="imageSpan";
+    img = document.createElement("img");
+    img.src="../assets/user.png";
+    sp.appendChild(img);
+    var commentText= document.createElement("div");
+    commentText.className="commentText";
+    div = document.createElement("div");
+    div.innerHTML="بهترین بازی تمام عمرم";
+    var date = document.createElement("div");
+    date.className="date";
+    date.innerHTML="دوشنبه ۱۱ آبان ۹۵";
+    commentText.appendChild(div);
+    commentText.appendChild(date);
+    comment.appendChild(sp);
+    comment.appendChild(commentText);
+    document.getElementById("commentsAndChats").appendChild(comment);
+}
+
+
+for(i=0 ; i< 5; i++){
+    comment=document.createElement("div");
+    comment.className="comment";
+    sp= document.createElement("span");
+    sp.className="imageSpan";
+    img = document.createElement("img");
+    img.src="../images/421645.jpg";
+    sp.appendChild(img);
+    commentText= document.createElement("div");
+    commentText.className="commentText";
+    div = document.createElement("div");
+    div.innerHTML="راهنمای بازی ۱";
+    var date = document.createElement("div");
+    date.className="date";
+    date.innerHTML="دوشنبه ۱۱ آبان ۹۵";
+    commentText.appendChild(div);
+    commentText.appendChild(date);
+    comment.appendChild(sp);
+    comment.appendChild(commentText);
+    document.getElementById("gameGuids").appendChild(comment);
+}
+
+
 $(document).ready(function() {
     $("#slider").owlCarousel({
         autoPlay: 3000, //Set AutoPlay to 3 seconds
@@ -23,33 +122,8 @@ $(document).ready(function() {
         pagination:true
 
     });
-
     $(".owl-controls").appendTo($("#pagination"));
-
-
-    // $.getJSON( "‫‪api.ie.ce-it.ir/F95/home‬‬", function( data ) {
-    //     var items = [];
-    //     $.each( data, function( key, val ) {
-    //         items.push( "<li id='" + key + "'>" + val + "</li>" );
-    //     });
-    //
-    //     $( "<ul/>", {
-    //         "class": "my-new-list",
-    //         html: items.join( "" )
-    //     }).appendTo( "body" );
-    // });
-
 });
-
-// $('.item')
-//     .on('mouseover', function(e) {
-//         e.preventDefault();
-//         $(this).hover(function () {
-//             $(this).css("background-color","yellow");
-//         });
-//         console.log($(this).index());
-//     })
-//
 
 $('#slider')
     .on('mouseover', function(e) {
